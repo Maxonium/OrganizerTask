2d3f39514d2e54002b5f6d5d837fbe5c0f5d8007
Maxim <maksimchugunov8@gmail.com>
2023-05-04 18:22:37 +0300

    Class "Task" is updated

--- src/main/java/org/mywork/Organizer/entity/Task.java
+++ src/main/java/org/mywork/Organizer/entity/Task.java
@@ -1,6 +1,9 @@
 package org.mywork.Organizer.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,8 @@ import javax.xml.crypto.Data;
 @Setter
 public class Task
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private Integer priorityId;
     private String description;
