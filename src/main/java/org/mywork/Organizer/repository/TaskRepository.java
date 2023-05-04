928d5ece1ac766a043068855c5e8896d1ac73bda
Maxim <maksimchugunov8@gmail.com>
2023-05-04 19:34:07 +0300

    Class "TaskRepo" have filled

--- src/main/java/org/mywork/Organizer/repository/TaskRepository.java
+++ src/main/java/org/mywork/Organizer/repository/TaskRepository.java
@@ -1,6 +1,11 @@
 package org.mywork.Organizer.repository;
 
-public class TaskRepository
+import org.mywork.Organizer.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer>
 {
 
 }
