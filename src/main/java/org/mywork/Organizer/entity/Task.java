0974cebe233fd00a60863cc04ac971bb7e07bea6
Maxim <maksimchugunov8@gmail.com>
2023-05-04 18:17:57 +0300

    Have filled class "Task"

--- src/main/java/org/mywork/Organizer/entity/Task.java
+++ src/main/java/org/mywork/Organizer/entity/Task.java
@@ -1,6 +1,22 @@
 package org.mywork.Organizer.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.xml.crypto.Data;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Task
 {
-
+    private Integer id;
+    private Integer priorityId;
+    private String description;
+    private Data data;
 }
